3a647ffb11a1eb267ba98b4ef41c666b4c778cbe
kenechilearnscode <kncokolo@gmail.com>
2016-08-06 17:31:38 +0200

    Initial Commit

---
+++ iOS10RichNotificationsTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS10RichNotificationsTutorial
+//
+//  Created by Kc on 2016-08-06.
+//  Copyright © 2016 Kenechi Okolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
